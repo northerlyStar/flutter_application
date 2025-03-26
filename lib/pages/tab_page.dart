ca2550c8230f82d6cc949d0d88010d3654347151
蒋闻铭 <wlgcjrcfl@gmail.com>
2025-03-26 22:51:14 +0800

    调整布局

--- lib/pages/tab_page.dart
+++ lib/pages/tab_page.dart
@@ -5,7 +5,73 @@ import 'package:flutter/material.dart';
 class TabPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return TabBarBox();
+    return DefaultTabController(
+        length: tabs.length,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text('Tab Bar'),
+          ),
+          drawer: DrawerBox(),
+          floatingActionButton: FloatingActionButton(
+            shape: CircleBorder(),
+            child: Icon(Icons.add),
+            onPressed: () {
+              ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(content: Text('you tap FloatingActionButton.')));
+            },
+          ),
+          floatingActionButtonLocation:
+              FloatingActionButtonLocation.centerDocked,
+          body: Column(
+            children: [
+              Card(
+                child: Column(
+                  children: [
+                    ListTile(
+                      leading: Icon(
+                        Icons.home,
+                        color: Colors.lightBlue,
+                      ),
+                      title: Text('三体科技有限公司',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w400,
+                          )),
+                      subtitle: Text('地球'),
+                    ),
+                    ListTile(
+                      leading: Icon(
+                        Icons.school,
+                        color: Colors.lightBlue,
+                      ),
+                      title: Text('奎因科技有限公司',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w400,
+                          )),
+                      subtitle: Text('地球'),
+                    ),
+                  ],
+                ),
+              ),
+              TabBar(
+                isScrollable: false,
+                tabs: tabs.map((item) {
+                  return Tab(
+                    text: item.text,
+                  );
+                }).toList(),
+              ),
+              Expanded(
+                child: TabBarView(
+                    children: tabs.map((item) {
+                  return Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: SelectedItem(item: item),
+                  );
+                }).toList()),
+              ),
+            ],
+          ),
+        ));
   }
 }
 
@@ -37,45 +103,6 @@ const List<ItemView> tabs = [
     icon: Icons.directions_boat,
   ),
 ];
-
-class TabBarBox extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return DefaultTabController(
-        length: tabs.length,
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text('Tab Bar'),
-            bottom: TabBar(
-              isScrollable: false,
-              tabs: tabs.map((item) {
-                return Tab(
-                  text: item.text,
-                );
-              }).toList(),
-            ),
-          ),
-          drawer: DrawerBox(),
-          floatingActionButton: FloatingActionButton(
-            shape: CircleBorder(),
-            child: Icon(Icons.add),
-            onPressed: () {
-              ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(content: Text('you tap FloatingActionButton.')));
-            },
-          ),
-          floatingActionButtonLocation:
-              FloatingActionButtonLocation.centerDocked,
-          body: TabBarView(
-              children: tabs.map((item) {
-            return Padding(
-              padding: EdgeInsets.all(8.0),
-              child: SelectedItem(item: item),
-            );
-          }).toList()),
-        ));
-  }
-}
 
 class DrawerBox extends StatelessWidget {
   @override
@@ -116,43 +143,10 @@ class SelectedItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final TextEditingController textTestController = TextEditingController();
-    textTestController.addListener(() {
-      // TODO
-      print('text changed.');
-    });
 
     return ListView(
       children: [
-        SizedBox(
-          height: 180,
-          child: Card(
-            child: Column(
-              children: [
-                ListTile(
-                  leading: Icon(
-                    Icons.home,
-                    color: Colors.lightBlue,
-                  ),
-                  title: Text('三体科技有限公司',
-                      style: TextStyle(
-                        fontWeight: FontWeight.w300,
-                      )),
-                  subtitle: Text('地球'),
-                ),
-                ListTile(
-                  leading: Icon(
-                    Icons.school,
-                    color: Colors.lightBlue,
-                  ),
-                  title: Text('奎因科技有限公司'),
-                  subtitle: Text('地球'),
-                ),
-              ],
-            ),
-          ),
-        ),
-        CardBox(textTestController: textTestController, item: item),
+        CardBox(item: item),
       ],
     );
   }
@@ -161,11 +155,9 @@ class SelectedItem extends StatelessWidget {
 class CardBox extends StatelessWidget {
   const CardBox({
     super.key,
-    required this.textTestController,
     required this.item,
   });
 
-  final TextEditingController textTestController;
   final ItemView item;
 
   @override
@@ -211,68 +203,6 @@ class CardBox extends StatelessWidget {
                 ScaffoldMessenger.of(context)
                     .showSnackBar(SnackBar(content: Text('click text button')));
               },
-            ),
-            SimpleDialog(
-              title: Center(child: Text('对话框标题')),
-              children: [
-                SimpleDialogOption(
-                  onPressed: () {
-                    ScaffoldMessenger.of(context)
-                        .showSnackBar(SnackBar(content: Text('点击第一个')));
-                  },
-                  child: Text('第一行信息'),
-                ),
-                SimpleDialogOption(
-                  onPressed: () {
-                    ScaffoldMessenger.of(context)
-                        .showSnackBar(SnackBar(content: Text('点击第二个')));
-                  },
-                  child: Text('第二行信息'),
-                ),
-              ],
-            ),
-            AlertDialog(
-              title: Text('警告！'),
-              titlePadding: EdgeInsets.all(15.0),
-              content: Column(
-                children: [
-                  Text('一旦被删除，将无法恢复'),
-                  TextField(
-                    controller: textTestController,
-                    maxLength: 20,
-                    maxLines: 1,
-                    autofocus: false,
-                    textAlign: TextAlign.center,
-                    onChanged: (value) {
-                      print('text is change to: $value');
-                    },
-                    onSubmitted: (value) {
-                      print('text is submit, value is: $value');
-                    },
-                    decoration: InputDecoration(
-                      fillColor: Colors.grey.shade100,
-                      filled: true,
-                      helperText: '用户名',
-                      prefixIcon: Icon(Icons.person),
-                      suffixText: '用户名',
-                    ),
-                  ),
-                ],
-              ),
-              contentPadding: EdgeInsets.all(12.0),
-              actions: [
-                TextButton(
-                    onPressed: () {
-                      print('确定删除');
-                    },
-                    child: Text('确定')),
-                TextButton(
-                    onPressed: () {
-                      print('取消删除');
-                    },
-                    child: Text('取消')),
-              ],
-              actionsPadding: EdgeInsets.all(8.0),
             ),
           ],
         ),
